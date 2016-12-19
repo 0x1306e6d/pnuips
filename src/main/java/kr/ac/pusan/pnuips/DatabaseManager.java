7112233cd0d9124104502395116a74efdc4571f1
Gihwan Kim <ghkim3221@gmail.com>
2016-12-20 00:10:24 +0900

    Apache Commons DBCP2 라이브러리 사용

--- src/main/java/kr/ac/pusan/pnuips/DatabaseManager.java
+++ src/main/java/kr/ac/pusan/pnuips/DatabaseManager.java
@@ -1,23 +1,46 @@
 package kr.ac.pusan.pnuips;
 
+import org.apache.commons.dbcp2.*;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
 
+    private static DataSource dataSource;
+    private static boolean setup = false;
+
     public static Connection getConnection() throws SQLException {
+        if (!setup) {
+            setup();
+        }
+        return dataSource.getConnection();
+    }
+
+    private synchronized static void setup() throws SQLException {
+        if (setup) {
+            return;
+        }
+
         try {
             Class.forName(DatabaseConstants.DRIVER);
         } catch (ClassNotFoundException e) {
             throw new SQLException(e);
         }
 
-        return DriverManager.getConnection(
+        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
                 DatabaseConstants.URL,
                 DatabaseConstants.USER,
-                DatabaseConstants.PASSWORD
-        );
+                DatabaseConstants.PASSWORD);
+        PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory, null);
+        ObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(poolableConnectionFactory);
+        poolableConnectionFactory.setPool(connectionPool);
+
+        dataSource = new PoolingDataSource<>(connectionPool);
+        setup = true;
     }
 
     private DatabaseManager() {
